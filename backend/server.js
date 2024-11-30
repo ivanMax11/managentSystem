75db15cc98fe52ae39c264a25233e1aaafbe1c06
Iván Monte <ivanmonte07.93@gmail.com>
2024-11-29 21:16:40 -0300

    frontend fixed in conections with servers

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ connectDB();
 app.use(cors({
     origin: ['http://localhost:5173', 'https://managentsystem.onrender.com', 'https://managentsystem-1.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   }));
   
 app.use(express.json());
